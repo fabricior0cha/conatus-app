7a203d6ae7f0220813b33cbd186c86bbc09b65b9
fabricior0cha <fabriciorochapalmeira@gmail.com>
2023-11-03 08:40:10 -0300

    feat: add enviroment base url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://app-conatus-service.onrender.com/'
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL
 })
 
 export default api
